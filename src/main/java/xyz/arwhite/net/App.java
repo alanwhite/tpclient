7943497356b480ab1a2da2b313962505c8672f6b
Alan White <alan@whitemail.net>
2023-01-14 16:57:08 +0000

    added TLS to proxy capability

--- src/main/java/xyz/arwhite/net/App.java
+++ src/main/java/xyz/arwhite/net/App.java
@@ -15,6 +15,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Hello world!
@@ -22,57 +28,77 @@ import java.net.http.HttpResponse.BodyHandlers;
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException, InterruptedException
-    {
-        System.out.println( "Hello World!" );
-        
-        // first make http proxy call to tinyproxy listening on 8888
-        
-        System.out.println("Java 11 http client direct to tinyproxy");
-        
-        var plainClient = HttpClient.newBuilder()
-        		.proxy(ProxySelector.of(new InetSocketAddress("localhost",8888)))
-        		.build();
-        
-        HttpRequest tinyGet = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://httpbin.org/ip")) 
-                .build();
-        
-        HttpResponse<String> tinyResp = plainClient.send(tinyGet, BodyHandlers.ofString());
-        
-        System.out.println(tinyResp.statusCode());
-        System.out.println(tinyResp.body());
-       
-        //-------------------
-        
-        System.out.println("pre-Java 11 http client direct to tinyproxy");
-        
-        URL weburl = new URL("https://httpbin.org/ip");
-        Proxy tinyProxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 8888));
-        HttpURLConnection connection = (HttpURLConnection) weburl.openConnection(tinyProxy);
-       
-        connection.setRequestMethod("GET");
+	public static void main( String[] args ) throws IOException, InterruptedException, KeyManagementException, CertificateException, KeyStoreException, NoSuchAlgorithmException
+	{
+		System.out.println( "Hello World!" );
+
+		// first make http proxy call to tinyproxy listening on 8888
+
+		System.out.println("Java 11 http client direct to tinyproxy");
+
+		var plainClient = HttpClient.newBuilder()
+				.proxy(ProxySelector.of(new InetSocketAddress("localhost",8888)))
+				.build();
+
+		HttpRequest tinyGet = HttpRequest.newBuilder()
+				.GET()
+				.uri(URI.create("https://httpbin.org/ip")) 
+				.build();
+
+		HttpResponse<String> tinyResp = plainClient.send(tinyGet, BodyHandlers.ofString());
+
+		System.out.println(tinyResp.statusCode());
+		System.out.println(tinyResp.body());
+
+		//-------------------
+
+		System.out.println("pre-Java 11 http client direct to tinyproxy");
+
+		URL weburl = new URL("https://httpbin.org/ip");
+		Proxy tinyProxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 8888));
+		HttpURLConnection connection = (HttpURLConnection) weburl.openConnection(tinyProxy);
+
+		connection.setRequestMethod("GET");
 		int responseCode = connection.getResponseCode();
 		System.out.println(responseCode);
-		
-		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		String inputLine;
 
-		while ((inputLine = in.readLine()) != null) {
-			System.out.println(inputLine);
+		if ( responseCode == 200 ) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println(inputLine);
+			}
+			in.close();
 		}
-		in.close();
 
-        //-------------------
-		
-        System.out.println("Java 11 http client TLS via stunnel to tinyproxy");
-        
-        System.out.println("NOT IMPLEMENTED YET");
-        
-        System.out.println("pre-Java 11 http client TLS via stunnel to tinyproxy");
-        
-        System.out.println("NOT IMPLEMENTED YET");
+		//-------------------
 
-    }
+		System.out.println("Java 11 http client TLS via stunnel to tinyproxy");
+
+		System.out.println("NOT IMPLEMENTED YET");
+
+		System.out.println("pre-Java 11 http client TLS via stunnel to tinyproxy");
+
+		var proxiedSocketFactory = new TLSProxiedSocketFactory("127.0.0.1", 3128, "/usr/local/etc/stunnel/stunnel.pem");
+
+		URL tlsurl = new URL("https://httpbin.org/ip");
+
+		HttpsURLConnection tlsConn = (HttpsURLConnection) tlsurl.openConnection();
+		tlsConn.setSSLSocketFactory(proxiedSocketFactory); // expects it to do the ssl negotiation....
+
+		tlsConn.setRequestMethod("GET");
+		int tlsRespCode = tlsConn.getResponseCode();
+		System.out.println(tlsRespCode);
+
+		if ( tlsRespCode == 200 ) {
+			BufferedReader tlsIn = new BufferedReader(new InputStreamReader(tlsConn.getInputStream()));
+			String tlsInputLine;
+
+			while ((tlsInputLine = tlsIn.readLine()) != null) {
+				System.out.println(tlsInputLine);
+			}
+			tlsIn.close();
+		}
+	}
 }
---
+++ src/main/java/xyz/arwhite/net/TLSProxiedSocketFactory.java
@@ -0,0 +1,157 @@
+package xyz.arwhite.net;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+public class TLSProxiedSocketFactory extends SSLSocketFactory {
+
+	private String tlsProxyHost;
+	private int tlsProxyPort;
+	private SSLSocketFactory sslSocketFactory;
+
+	public TLSProxiedSocketFactory(String tlsProxyHost, int tlsProxyPort) throws NoSuchAlgorithmException {
+		this.tlsProxyHost = tlsProxyHost;
+		this.tlsProxyPort = tlsProxyPort;
+		this.sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+	}
+	
+	public TLSProxiedSocketFactory(String tlsProxyHost, int tlsProxyPort, String cacertPEMFile) 
+			throws FileNotFoundException, IOException, CertificateException, 
+			KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+		
+		this.tlsProxyHost = tlsProxyHost;
+		this.tlsProxyPort = tlsProxyPort;
+		
+		try( var pemReader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(cacertPEMFile))) ) {
+			var certFactory = CertificateFactory.getInstance("X.509");
+			
+			List<String> certStrings = new ArrayList<>();
+			StringBuilder certData = new StringBuilder();
+			String line = pemReader.readLine();
+			while( line != null ) {
+				if ( line.contains("BEGIN CERTIFICATE") ) 
+					certData = new StringBuilder();
+				else if ( line.contains("END CERTIFICATE")) 
+					certStrings.add(certData.toString());
+				else
+					certData.append(line);
+				
+				line = pemReader.readLine();
+			}
+			
+			if ( certStrings.isEmpty() )
+				throw(new IllegalArgumentException("no certs found in trust PEM"));
+			
+			KeyStore proxyTrustStore = KeyStore.getInstance("pkcs12");
+			proxyTrustStore.load(null,null);
+			
+			for ( String certString : certStrings ) {
+				byte[] certBytes = Base64.getDecoder().decode(certString);
+				var cert = certFactory.generateCertificate(new ByteArrayInputStream(certBytes));
+				proxyTrustStore.setCertificateEntry(Integer.toString(cert.hashCode()), cert);
+			}
+			
+			var tmf = TrustManagerFactory.getInstance("PKIX");
+			tmf.init(proxyTrustStore);
+			
+			var sslContext = SSLContext.getInstance("TLSv1.3");
+			sslContext.init(null, tmf.getTrustManagers(), null);
+			
+			sslSocketFactory = sslContext.getSocketFactory();
+		} 
+	}
+	
+	@Override
+	public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+
+		SSLSocket proxy = (SSLSocket) sslSocketFactory.createSocket(this.tlsProxyHost, this.tlsProxyPort);
+
+		proxy.startHandshake();
+
+		String connect = "CONNECT " + host + ":" + port + " HTTP/1.1\n";
+		proxy.getOutputStream().write(connect.getBytes());
+
+		//				if ( !proxyUser.isBlank() ) {
+		//					var creds = proxyUser + ":" + proxyPass;
+		//					String auth = "Proxy-Authorization: Basic " 
+		//							+ Base64.getEncoder().encodeToString(creds.getBytes()) + "\n";
+		//					remoteSocket.getOutputStream().write(auth.getBytes());
+		//				}
+
+		proxy.getOutputStream().write("\n".getBytes());
+
+		var reader = new BufferedReader(
+				new InputStreamReader(proxy.getInputStream(), "US-ASCII"));
+		var httpResponse = reader.readLine();
+		var words = httpResponse.split(" ");
+		if ( words.length < 2 || !"200".equals(words[1]) ) {
+			throw(new IOException("Unhandled response from proxy "+httpResponse));
+		}
+
+		// drain and ignore any response headers
+		int len = -1;
+		while ( len != 0 ) {
+			var line = reader.readLine();
+			len = line.length();
+		}
+
+		// we now need to do the TLS negotiation with the proxied endpoint .....
+		
+		return proxy;
+	}
+
+	@Override
+	public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
+		return createSocket(null, host, port, true);
+	}
+
+	@Override
+	public Socket createSocket(String host, int port, InetAddress localHost, int localPort)
+			throws IOException, UnknownHostException {
+		return createSocket(null, host, port, true);
+	}
+
+	@Override
+	public Socket createSocket(InetAddress host, int port) throws IOException {
+		return createSocket(null, host.getHostName(), port, true);
+	}
+
+	@Override
+	public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort)
+			throws IOException {
+		return createSocket(null, address.getHostName(), port, true);
+	}
+
+	@Override
+	public String[] getDefaultCipherSuites() {
+		return sslSocketFactory.getDefaultCipherSuites();
+	}
+
+	@Override
+	public String[] getSupportedCipherSuites() {
+		return sslSocketFactory.getSupportedCipherSuites();
+	}
+
+}
