0d200a84577f6b01d6c9c275488cd7dc798f10d6
Alan White <alan@whitemail.net>
2023-01-15 12:39:19 +0000

    start work on new client

--- src/main/java/xyz/arwhite/net/App.java
+++ src/main/java/xyz/arwhite/net/App.java
@@ -2,10 +2,8 @@ package xyz.arwhite.net;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.ProxySelector;
@@ -30,58 +28,78 @@ public class App
 {
 	public static void main( String[] args ) throws IOException, InterruptedException, KeyManagementException, CertificateException, KeyStoreException, NoSuchAlgorithmException
 	{
-//		System.out.println( "Hello World!" );
-//
-//		// first make http proxy call to tinyproxy listening on 8888
-//
-//		System.out.println("Java 11 http client direct to tinyproxy");
-//
-//		var plainClient = HttpClient.newBuilder()
-//				.proxy(ProxySelector.of(new InetSocketAddress("localhost",8888)))
-//				.build();
-//
-//		HttpRequest tinyGet = HttpRequest.newBuilder()
-//				.GET()
-//				.uri(URI.create("https://httpbin.org/ip")) 
-//				.build();
-//
-//		HttpResponse<String> tinyResp = plainClient.send(tinyGet, BodyHandlers.ofString());
-//
-//		System.out.println(tinyResp.statusCode());
-//		System.out.println(tinyResp.body());
-//
-//		//-------------------
-//
-//		System.out.println("pre-Java 11 http client direct to tinyproxy");
-//
-//		URL weburl = new URL("https://httpbin.org/ip");
-//		Proxy tinyProxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 8888));
-//		HttpURLConnection connection = (HttpURLConnection) weburl.openConnection(tinyProxy);
-//
-//		connection.setRequestMethod("GET");
-//		int responseCode = connection.getResponseCode();
-//		System.out.println(responseCode);
-//
-//		if ( responseCode == 200 ) {
-//			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//			String inputLine;
-//
-//			while ((inputLine = in.readLine()) != null) {
-//				System.out.println(inputLine);
-//			}
-//			in.close();
-//		}
+		System.out.println( "Hello World!" );
+
+		// first make http proxy call to tinyproxy listening on 8888
+
+		System.out.println("Java 11 http client direct to tinyproxy");
+
+		var plainClient = HttpClient.newBuilder()
+				.proxy(ProxySelector.of(new InetSocketAddress("localhost",8888)))
+				.build();
+
+		HttpRequest tinyGet = HttpRequest.newBuilder()
+				.GET()
+				.uri(URI.create("https://httpbin.org/ip")) 
+				.build();
+
+		HttpResponse<String> tinyResp = plainClient.send(tinyGet, BodyHandlers.ofString());
+
+		System.out.println(tinyResp.statusCode());
+		System.out.println(tinyResp.body());
+
+		//-------------------
+
+		System.out.println("pre-Java 11 http client direct to tinyproxy");
+
+		URL weburl = new URL("https://httpbin.org/ip");
+		Proxy tinyProxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 8888));
+		HttpURLConnection connection = (HttpURLConnection) weburl.openConnection(tinyProxy);
+
+		connection.setRequestMethod("GET");
+		int responseCode = connection.getResponseCode();
+		System.out.println(responseCode);
+
+		if ( responseCode == 200 ) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println(inputLine);
+			}
+			in.close();
+		}
 
 		//-------------------
 
 		System.out.println("Java 11 http client TLS via stunnel to tinyproxy");
 
-		System.out.println("NOT IMPLEMENTED YET");
+		// work in progress
+
+		var stunClient = HttpClient.newBuilder()
+				.build();
+
+		HttpRequest stunGet = HttpRequest.newBuilder()
+				.GET()
+				.uri(URI.create("https://httpbin.org/ip")) 
+				.build();
+
+		HttpResponse<String> stunResp = stunClient.send(stunGet, BodyHandlers.ofString());
+
+		System.out.println(stunResp.statusCode());
+		System.out.println(stunResp.body());
+
+		//-------------------
 
 		System.out.println("pre-Java 11 http client TLS via stunnel to tinyproxy");
 
 		var proxiedSocketFactory = new TLSProxiedSocketFactory("127.0.0.1", 3128, "/usr/local/etc/stunnel/stunnel.pem");
-
+		
+//		var creds = "user:password";
+//		String auth = "Proxy-Authorization: Basic " + 
+//				Base64.getEncoder().encodeToString(creds.getBytes()) + "\n";
+//		proxiedSocketFactory.setProxyHeader(auth);
+		
 		URL tlsurl = new URL("https://httpbin.org/ip");
 
 		HttpsURLConnection tlsConn = (HttpsURLConnection) tlsurl.openConnection();
